19608ebfce80b657b6b23f81afe1942cd8ffe4f5
MichelBEHDB <145080519+MichelBEHDB@users.noreply.github.com>
2024-11-12 22:47:49 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import funciones_aux as faux
-from data import datos
+from data import data
 
 tam = (10, 10)
 cuadricula = np.random.randint(0, 256, (tam[0], tam[1], 3), dtype=int)
@@ -14,8 +14,8 @@ alpha_inicial = 0.5
 radio_inicial = max(cuadricula.shape) / 2 
 
 for iteracion in range(total_iteraciones):
-    color_entrada = datos[np.random.randint(len(datos))]
-    ganador = faux.neurona_ganadora(color_entrada, cuadricula)
-    alpha = faux.tasa_aprendizaje(iteracion, total_iteraciones, alpha_inicial)
-    radio_vecindad = radio_inicial
-    faux.actualizar_pesos(cuadricula, color_entrada, ganador, alpha, radio_vecindad)
+    for color_entrada in data:  # Iterar sobre todos los colores en el conjunto de datos
+        ganador = faux.neurona_ganadora(color_entrada, cuadricula)
+        alpha = faux.tasa_aprendizaje(iteracion, total_iteraciones, alpha_inicial)
+        radio_vecindad = radio_inicial * (1 - iteracion / total_iteraciones)
+        cuadricula = faux.actualizar_pesos(cuadricula, color_entrada, ganador, alpha, radio_vecindad)
